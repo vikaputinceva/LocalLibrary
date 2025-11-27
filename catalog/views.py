792716a1852e7c9019681961755a65323a031741
vikaputinceva <vikaputin2008@gmail.com>
2025-11-27 12:56:17 +0700

    Sessions framework

--- catalog/views.py
+++ catalog/views.py
@@ -5,18 +5,24 @@ from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
-    # Генерация "количеств" некоторых главных объектов
+
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
-    # Доступные книги (статус = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
 
-    # Отрисовка HTML-шаблона index.html с данными внутри
-    # переменной контекста context
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
     return render(
         request,
         'index.html',
